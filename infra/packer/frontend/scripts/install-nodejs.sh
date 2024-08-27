a3f14a591983366dcfe40af1f9bdd5806eaceaf5
Fabien ALLADE <alladefabien@gmail.com>
2024-08-27 15:51:46 +0200

    work on nodejs installation add source in configuration

--- infra/packer/frontend/scripts/install-nodejs.sh
+++ infra/packer/frontend/scripts/install-nodejs.sh
@@ -12,7 +12,7 @@ apt-get update -y
 # installs nvm (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 
-source ~/.bashrc
+source /home/ubuntu/.bashrc
 
 # download and install Node.js (you may need to restart the terminal)
 nvm install 20
