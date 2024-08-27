cbdddb7a5ff599824f4fd1ca647366d7053f3714
Fabien ALLADE <alladefabien@gmail.com>
2024-08-27 15:38:29 +0200

    work on nodejs installation

--- infra/packer/frontend/scripts/install-nodejs.sh
+++ infra/packer/frontend/scripts/install-nodejs.sh
@@ -1,10 +1,22 @@
 # bin/bash
 apt-get update -y
-apt-get upgrade -y
+#apt-get upgrade -y
+#
+#apt-get install curl -y
+#
+#apt-get install nodejs -y
+#
+##apt-get install npm -y
+#npm install -g pm2
 
-apt-get install curl -y
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 
-apt-get install nodejs -y
+# download and install Node.js (you may need to restart the terminal)
+nvm install 20
 
-#apt-get install npm -y
-npm install -g pm2
+# verifies the right Node.js version is in the environment
+node -v # should print `v20.17.0`
+
+# verifies the right npm version is in the environment
+npm -v # should print `10.8.2`
