ed0d1134cd956ef6f27ec53abccfb9a1049ee2bd
Fabien ALLADE <alladefabien@gmail.com>
2024-08-30 06:11:00 +0200

    work on monitoring config in terraform

--- infra/terraform/monitoring.tf
+++ infra/terraform/monitoring.tf
@@ -14,7 +14,7 @@ data "aws_iam_policy_document" "vpc_assume_role" {
 }
 
 resource "aws_iam_role" "vpc" {
-  name               = "${var.application_name}-${var.environment_name}-networks"
+  name               = "${var.application_name}-${var.environment_name}-network"
   assume_role_policy = data.aws_iam_policy_document.vpc_assume_role.json
 }
 
