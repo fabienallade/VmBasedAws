4ea5f836a28c660462cddfcffb1cfb66339885db
Fabien ALLADE <alladefabien@gmail.com>
2024-08-30 05:40:40 +0200

    work on monitoring config in terraform

--- infra/terraform/monitoring.tf
+++ infra/terraform/monitoring.tf
@@ -1,35 +1,35 @@
-# data "aws_iam_policy_document" "vpc_assume_role" {
-#   version = "2012-10-17"
-#   statement {
-#     effect = "Allow"
-#
-#     principals {
-#       identifiers = ["vpc-flow-logs.amazonaws.com"]
-#       type        = "Service"
-#     }
-#
-#     actions = ["sts:AssumeRole"]
-#   }
-# }
-#
-# resource "aws_iam_role" "vpc" {
-#   name               = "${var.application_name}-${var.environment_name}-network"
-#   assume_role_policy = data.aws_iam_policy_document.vpc_assume_role.json
-# }
-#
-# data "aws_iam_policy_document" "cloudwatch" {
-#   version = "2012-10-17"
-#   statement {
-#     effect = "Allow"
-#     actions = [
-#       "logs:CreateLogGroup",
-#     ]
-#     resources = ["*"]
-#   }
-# }
-#
-# resource "aws_iam_role_policy" "cloudwatch" {
-#   name   = "${var.application_name}-${var.environment_name}-network-cloudwatch"
-#   policy = data.aws_iam_policy_document.cloudwatch.json
-#   role   = aws_iam_role.vpc.id
-# }
+data "aws_iam_policy_document" "vpc_assume_role" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "vpc" {
+  name               = "${var.application_name}-${var.environment_name}-network"
+  assume_role_policy = data.aws_iam_policy_document.vpc_assume_role.json
+}
+
+data "aws_iam_policy_document" "cloudwatch" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "cloudwatch" {
+  name   = "${var.application_name}-${var.environment_name}-network-cloudwatch"
+  policy = data.aws_iam_policy_document.cloudwatch.json
+  role   = aws_iam_role.vpc.id
+}
